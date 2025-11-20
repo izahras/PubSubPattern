27ba6bfd1aea12fa5d2943f3474ce73fa327acb7
zahra sadeghi <zahrasadeghis82@gmail.com>
2025-11-21 03:26:58 +0330

    Delete Runner

--- run-example.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-REM اسکریپت Batch برای اجرای برنامه با تنظیمات UTF-8
-chcp 65001 >nul
-echo اجرای برنامه نمونه Pub-Sub...
-echo.
-dotnet run --project PubSub.Example/PubSub.Example.csproj
-pause
-
